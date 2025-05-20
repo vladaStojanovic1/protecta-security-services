16e7b8d0d9e57a9ba9629409fef4bbf3cdaec830
vladasto <1vladastojanovic1@gmail.com>
2025-05-20 20:51:00 +0200

    Added favicon

--- wp-content/themes/vladastheme/header.php
+++ wp-content/themes/vladastheme/header.php
@@ -35,12 +35,12 @@ global $globalSite;
     <link rel="profile" href="http://gmpg.org/xfn/11">
 
     <!--Favicon-->
-<!--    <link rel="icon" type="image/png" href="--><?php //echo get_template_directory_uri() . '/src/images/favicon/favicon-96x96.png'; ?><!--" sizes="96x96" />-->
-<!--    <link rel="icon" type="image/svg+xml" href="--><?php //echo get_template_directory_uri() . '/src/images/favicon/favicon.svg'; ?><!--" />-->
-<!--    <link rel="shortcut icon" href="--><?php //echo get_template_directory_uri() . '/src/images/favicon/favicon.ico'; ?><!--" />-->
-<!--    <link rel="apple-touch-icon" sizes="180x180" href="--><?php //echo get_template_directory_uri() . '/src/images/favicon/apple-touch-icon.png'; ?><!--" />-->
-<!--    <meta name="apple-mobile-web-app-title" content="Vojkan ALU & PVC" />-->
-<!--    <link rel="manifest" href="--><?php //echo get_template_directory_uri() . '/src/images/favicon/site.webmanifest'; ?><!--" />-->
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon-96x96.png'; ?>" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon.svg'; ?>" />
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri() . '/src/images/favicon/favicon.ico'; ?>" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/src/images/favicon/apple-touch-icon.png'; ?>" />
+    <meta name="apple-mobile-web-app-title" content="Protecta security services" />
+    <link rel="manifest" href="<?php echo get_template_directory_uri() . '/src/images/favicon/site.webmanifest'; ?>" />
 </head>
 
 <body <?php body_class(); ?>>
